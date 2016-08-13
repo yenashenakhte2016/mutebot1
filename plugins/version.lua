395ca4308621e1a68145d65ca4ffdc3d5db831bd
yenashenakhte2016 <mmnsfyrad@gmail.com>
2016-08-14 01:49:24 +0430

    Add files via upload

--- plugins/version.lua
+++ plugins/version.lua
@@ -14,12 +14,14 @@
 do
 
 function run(msg, matches)
-  return 'DBTeam Bot V1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot\n\nDevelopers: @MaSkAoS @Josepdal\nSupport: @skneos @iicc1 @Serx666\nChannels: @DBTeamEN @DBTeamES\n\nCheckout: https://github.com/Josepdal/DBTeam\nGNU GPL v2 license.'
+  return 'Security Bot V1 Supergroups\nAn advanced Administration bot \n\nDevelopers: @MohammadDeveloper @Mutepuker\nChannel: @MuteTeam'
 end
 
 return {
   patterns = {
-    "^#version$"
+    "^#version$",
+    "^!version$",
+    "^/version$"
   }, 
   run = run 
 }
