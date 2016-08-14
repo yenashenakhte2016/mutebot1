194f84f1a3283f4d7f1ec03475c180d3d2af37b7
yenashenakhte2016 <mmnsfyrad@gmail.com>
2016-08-14 19:59:20 +0430

    Add files via upload

---
+++ plugins/emoji.lua
@@ -0,0 +1,51 @@
+local function run(msg, matches)
+    if not permissions(msg.from.id, msg.to.id, "settings") then
+        local hash = 'emoji:'..msg.to.id
+        if redis:get(hash) then
+            delete_msg(msg.id, ok_cb, false)
+            send_report(msg)
+        end
+    end
+end
+
+return {
+patterns = {
+    "😄",
+    "😃",
+    "😂",
+    "😁",
+    "😀",
+    "😊",
+    "☺" ,
+    "😐",
+    "😯",
+    "😉",
+    "😈",
+    "😇",
+    "😆",
+    "😅",
+    "😴",
+    "😢",
+    "😡",
+    "😬",
+    "😠",
+    "😕",
+    "😑",
+    "😨",
+    "😧",
+    "😦",
+    "😥",
+    "😤",
+    "😣",
+    "😮",
+    "😍",
+    "😎",
+    "😭",
+    "😓",
+    "😱",
+    "😚",
+    "😗",
+    "😷",
+    "😫",
+    "😪"
+}, run = run}
