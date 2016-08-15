f6070428b29b17048863fbe70d2dc07196b6d413
yenashenakhte2016 <mmnsfyrad@gmail.com>
2016-08-16 01:34:12 +0430

    Add files via upload

--- steady.sh
+++ steady.sh
@@ -43,7 +43,7 @@ VOLUNTARY=1
 VOLUNTARYCHECK=0
 I=1
 BOT=mutebot1  # You can put here other bots. Also you can change it to run more than one bot in the same server.
-RELOADTIME=10  # Time between checking cpu calls of the cli process. Set the value high if your bot does not receive lots of messages.
+RELOADTIME=2  # Time between checking cpu calls of the cli process. Set the value high if your bot does not receive lots of messages.
 
 
 function tmux_mode {
